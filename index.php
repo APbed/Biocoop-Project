79ec6942dc7414c81109215bf670fc5881438e6c
AxelArcidiaco <axel.arcidiaco@gmail.com>
2021-05-18 16:20:57 +0200

    Merge origin/master
    Conflicts:
    	index.php

--- index.php
+++ index.php
@@ -14,6 +14,7 @@ and open the template in the editor.
         <?php
         // put your code here
         echo 'Tout fonctionne bien !';
+        echo 'modifications du fichier index.php';
         ?>
     </body>
 </html>
